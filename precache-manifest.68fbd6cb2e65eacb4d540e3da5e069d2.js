913c518f79cc520a6b796124156ababc3a38f716
Akshat Kumar Agarwal <akshat.16@outlook.com>
2020-04-29 23:08:35 +0530

    Updates

---
+++ precache-manifest.68fbd6cb2e65eacb4d540e3da5e069d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484f5701c7bc176ea2957e2cedbaa108",
+    "url": "/corona-tracker/index.html"
+  },
+  {
+    "revision": "98beb4dfb99b65a1b1e2",
+    "url": "/corona-tracker/static/css/main.8d0b4032.chunk.css"
+  },
+  {
+    "revision": "69ae80b38b588798c68b",
+    "url": "/corona-tracker/static/js/2.57a9c20b.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/corona-tracker/static/js/2.57a9c20b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98beb4dfb99b65a1b1e2",
+    "url": "/corona-tracker/static/js/main.c65279ca.chunk.js"
+  },
+  {
+    "revision": "4127b310accafee69bc6",
+    "url": "/corona-tracker/static/js/runtime-main.cfc71fe3.js"
+  },
+  {
+    "revision": "bc08d51c1c8972bf848ceb18a84b0d9e",
+    "url": "/corona-tracker/static/media/logo.bc08d51c.png"
+  }
+]);
